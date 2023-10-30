5fed03f908da5d8f0a6f23712d4566a8a576b6a6
rania_farrag <rania.f@aerbag.app>
2023-10-30 23:52:48 +0200

    Authentication

---
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\AuthenticationService;
+use Faker\Provider\Base;
+use Illuminate\Http\Request;
+
+class RegisterController extends BaseController
+{
+    public AuthenticationService $authenticationService;
+
+    public function __con
+
+    public function register(){
+
+        $user =
+    }
+
+    public function login(){
+
+    }
+}
